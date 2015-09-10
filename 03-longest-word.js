df706745336a50049ace28e41d2d87495eac3711
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-09-10 02:11:40 -0500

    find longest word using reduce

--- 03-longest-word.js
+++ 03-longest-word.js
@@ -1,18 +1,12 @@
+// Using the reduce method
+
 function LongestWord(sen) {
-  var arr = sen.split(" ");
-  arr.reduce(function (prev,next){
-   if (next.length > prev.length) return next;
-    return prev;
-  };
+  var arr = sen.split(" ")
+  var long = arr.reduce(function (prev,next) {
+    if (next.length > prev.length) return next
+    return prev
+  })
+  return long
 }
 
-console.log( LongestWord( "What the hell is going on" ) );
-
-
-
-// var longestname = sk8.reduce(function (prev,next){
-//  if (next.length > prev.length) return next;
-//   return prev;
-// });
-//
-// console.log( longestname );
+console.log( LongestWord( "What the hell is going on" ) )
