a442eb11bfa88ab6c6337f5a3a35adfc5078d55c
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-09-19 00:19:09 -0500

    adding notes

--- easy/02-First-Factorial.js
+++ easy/02-First-Factorial.js
@@ -36,3 +36,21 @@ console.log( FirstFactorial( 8 ) )
 // Scratch pad
 // ----------------------------------------------------------------------------
 // I see that there is a recursive way to do this, but recursion doesn't make sense to me yet.
+
+
+
+
+
+
+
+
+//welcome to recursion!
+//recursion is simply calling a function from within itself to rerun the function
+//it's like a complex for loop a lot of the time, but is also super useful for binary searchs
+
+// function FirstFactorial(num) {
+//   if( num === 1 ) { //recursion needs a base case, otherwise it will go on forever
+//     return 1;       //if num is ever 1 we break out of the recursion
+//   }
+//   return num * FirstFactorial(num-1); //recursion magic! for factorials (num!) the answer is num * num-1 ... * 1
+// }                                     //so we need to call FirstFactorial on all the numbers below num and collect the results       
